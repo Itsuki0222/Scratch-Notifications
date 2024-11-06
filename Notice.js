8587eb44e6e74372b2bcc13236b0b956f1372d18
Itsuki0222 <175463719+Itsuki0222@users.noreply.github.com>
2024-11-06 21:55:24 +0900

    Update Notice.js

--- Notice.js
+++ Notice.js
@@ -1,31 +1,12 @@
-class Notice {
-    constructor() {}
-    getInfo() { // 拡張機能の各種情報
-        return {
-            id: 'Notice',
-            name: 'Notice', // 拡張機能の名前
-            blocks: [ // 各ブロックの定義
-            {
-                opcode: 'Notice', // このブロックが実行されると、クラス内のhelloというメソッドが呼ばれることを意味しています
-                blockType: Scratch.BlockType.COMMAND,　// 「10歩動かす」のような通常の命令ブロック
-                text: 'Notice' // ブロックに表示されるテキスト
-            }
-            ]
-        }
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://corsproxy.io/?https%3A%2F%2Fapi.scratch.mit.edu%2Fusers%2FLinux-zen%2Fmessages%2Fcount");
+xhr.send();
+xhr.responseType = "json";
+xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        const data = xhr.response;
+        alert(data.count);
+    } else {
+        console.log(`Error: ${xhr.status}`);
     }
-    Notice() {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://api.scratch.mit.edu/users/Linux-zen/messages/count");
-        xhr.send();
-        xhr.responseType = "json";
-        xhr.onload = () => {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                const data = xhr.response;
-                alert(data.count);
-            } else {
-                console.log(`Error: ${xhr.status}`);
-            }
-        };
-    }
-}
-Scratch.extensions.register(new Notice());
+};
